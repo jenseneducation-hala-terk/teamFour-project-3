22d510600ca8944905a54da7b4325dcc149b112b
Aleksander Dimitrijevic <dimitrijevicaleksander.dev@gmail.com>
2019-12-05 12:52:59 +0100

    initial commit, created shell project with necessary folders and files

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+app.set("view engine", "ejs");
+app.use("/static", express.static("public"));
+
+// Routes
+app.get("/", (req, res) => {
+  res.render("index");
+});
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+app.get("/contact", (req, res) => {
+  res.render("contact");
+});
+app.get("/signup", (req, res) => {
+  res.render("signup");
+});
+app.get("/signin", (req, res) => {
+  res.render("signin");
+});
+
+app.listen(3000);
+console.log("Server is running...");
